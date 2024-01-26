71c9f6c250c4ea4fafd88d1d47ffa7691939d171
Jan Michael Biyo <janmichael.biyo@gmail.com>
2024-01-26 15:15:10 -0500

    did most of the route work will see if it works

--- database.sql
+++ database.sql
@@ -9,5 +9,10 @@ CREATE TABLE "gallery" (
 INSERT INTO "gallery" 
 ("url", "title", "description")
 VALUES
-('images/goat_small.jpg', 'Goat!', 'Photo of a goat taken at Glacier National Park.');
+('images/goat_small.jpg', 'Goat!', 'Photo of a goat taken at Glacier National Park.'),
+('images/kinsey.png', 'Kinsey', 'The golden retriver husky'),
+('images/Master_Sword.jpeg', 'Master Sword', 'The legendary heros sword the bane of darkness'),
+('images/mushroom.jpg', 'Mushroom', 'Power mushroom can make user feel triple in size or so they say'),
+('images/sad_Shredder.jpeg', 'Sad Shredder', 'Portrait of Foot clan leader after loosing to turtles again'),
+('images/totoro.png', 'Totoro', 'The impresive forest deity/monster as least some would say');
   
--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../modules/pool.js');
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
   // code here
+  const galleryId = req.params.id;
+  const queryGalleryUpdate = `UPDATE "gallery" SET "likes" = $1 WHERE "id" = $2;`;
+  pool
+    .query(queryGalleryUpdate, [req.body.likes, galleryId]);
+    .then((response) => {
+        res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log('this error belongs to put route', error);
+      res.sendStatus(500);
+    });
 });
 
 // GET /gallery
 router.get('/', (req, res) => {
   // code here
+  const queryGallery = 'SELECT * FROM "gallery";';
+  pool
+    .query(queryGallery)
+    .then((result) => {
+      console.log('this is the list', result);
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log('This is an error on the get route', error);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
