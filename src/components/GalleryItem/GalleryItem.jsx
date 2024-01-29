1b81ca96c2291755939fe65e755186af338e7259
Jan Michael Biyo <janmichael.biyo@gmail.com>
2024-01-29 11:28:21 -0500

    i added an add like buttons now we will try and work on the update feature

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -4,13 +4,23 @@ import { useState } from 'react';
 import './GalleryItem.css';
 
 function GalleryItem({ galleryData }) {
+  const [galleryPicture, setGalleryPicture] = useState([]);
   return (
     <div data-testid="galleryItem" className="galleryItem">
-      <img src={galleryData.url} />
+      <div onClick={() => setGalleryPicture(!galleryPicture)}>
+        {galleryPicture ? (
+          <img src={galleryData.url} />
+        ) : (
+          <p> {galleryData.description} </p>
+        )}
+      </div>
 
-      <p> {galleryData.descriptions} </p>
+      <p> {galleryData.title} </p>
 
       <p> {galleryData.likes}</p>
+      <button onClick={(event) => updateLikes(galleryData.id)}>
+        You Like That!
+      </button>
     </div>
   );
 }
